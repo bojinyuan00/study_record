6626f90c6e7d015a6947c78aef5b83ba1b4b283b
bujinyuan <627178181@qq.com>
2019-03-07 11:57:31 +0800

    添加bootstrap知识点记录，或许会用到

---
+++ bootstrap/bootstrap知识点记录.php
@@ -0,0 +1,18 @@
+<?php
+
+1、bootstrap btn 按钮颜色
+	'白色'       'btn'，              	==>'原始'
+
+	​'浅蓝色'     'btn btn-primary'，		==>'原始'
+
+	​'深蓝色'     'btn btn-info'，		==>'信息/首选'
+
+	​'绿色'       'btn btn-success'，		==>'成功'
+
+	​'黄色'       'btn btn-warning'，		==>'警告'
+
+	​'红色'       'btn btn-danger'，		==>'危险'
+
+	​'黑色'       'btn btn-inverse'，		==>'链接'
+
+
