94348b2ab405580f729cb43f26b51bbacfeca120
bujinyuan <627178181@qq.com>
2019-03-15 15:22:01 +0800

    新增 关联模型查询时的特定字段选择

--- laravel/常用到的方法.php
+++ laravel/常用到的方法.php
@@ -286,6 +286,15 @@ if ($request->filled('train_status')) {
         return getJson(0, null, $workitems, count($workitems));
     }
 
+    $data_project= Project::query()
+                ->whereIn('id', $project_ids)
+                ->select('id','title','video_account','video_account_password')
+                ->with(['video_device' => function($query){
+                    $query->select('id', 'name', 'device_num as num', 'type', 'is_online','project_id');
+                }])
+                ->get()
+                ->toArray();
+
 //with 关联模型内继续使用连表查询
 //获取施工计划列表
     public function get_construction_progresses(Request $request)
