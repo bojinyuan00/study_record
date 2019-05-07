272c69bc7b22f29ac3097ddf54363759ee12edbb
bujinyuan <627178181@qq.com>
2019-05-07 17:39:01 +0800

    新增    多条件查询

--- laravel/常用到的方法.php
+++ laravel/常用到的方法.php
@@ -418,3 +418,14 @@ try {
         } catch (\Exception $e) {
             return getJson(-100, $e->getMessage());
         }
+
+
+//查询时  使用闭包增减筛选条件
+DB::table('users as u')
+->select('u.user_id','c.class')
+->leftJoin('class as c', function($join)
+{
+    $join->on('c.user_id', '=', 'u.user_id')
+    ->on('c.status', '=', '2');
+})
+->get();
